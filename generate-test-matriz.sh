c97d628e732790f6b03a45bf673cde9fb9fd7df3
J040-V1T0R <Joaovitordagracaamerico@gmail.com>
2025-07-03 23:53:01 -0300

    arrumando o script da matriz

--- generate-test-matriz.sh
+++ generate-test-matriz.sh
@@ -1,39 +1,37 @@
 #!/bin/bash
 
-# Gera um tamanho aleatório da matriz entre 2 e 5
+# Gera um tamanho aleatório para a matriz entre 2 e 5
 N=$((RANDOM % 4 + 2))
 
-# Lista de operações possíveis
+# Operações possíveis
 OPERACOES=("a" "s" "m")
-
-# Escolhe uma operação aleatória
 OPERACAO=${OPERACOES[$RANDOM % ${#OPERACOES[@]}]}
 
 # Cria o arquivo de entrada
 echo $N > input.txt
 
-# Gera a primeira matriz
+# Gera primeira matriz (N x N)
 for ((i = 0; i < N * N; i++)); do
     echo $((RANDOM % 21 - 10)) >> input.txt
 done
 
-# Gera a segunda matriz
+# Gera segunda matriz (N x N)
 for ((i = 0; i < N * N; i++)); do
     echo $((RANDOM % 21 - 10)) >> input.txt
 done
 
-# Adiciona a operação
+# Adiciona operação no final
 echo $OPERACAO >> input.txt
 
-# Compila o programa
+# Compila o programa usando o Makefile
 make matriz > /dev/null
 
-# Executa com o input e salva a saída
+# Executa o programa com o input.txt e salva a saída
 ./matriz < input.txt > output.txt
 
-# Mostra a entrada e saída
-echo "Entrada de teste:"
+# Mostra a entrada e a saída
+echo "=== Entrada de teste (input.txt) ==="
 cat input.txt
 
-echo -e "\nSaída produzida:"
+echo -e "\n=== Saída produzida pelo programa (output.txt) ==="
 cat output.txt
