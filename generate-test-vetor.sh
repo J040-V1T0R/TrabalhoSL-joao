7da90fde2749b4650b55255bd070474fcb95d11c
J040-V1T0R <Joaovitordagracaamerico@gmail.com>
2025-07-04 00:28:07 -0300

    arrumando os scripts vetor

--- generate-test-vetor.sh
+++ generate-test-vetor.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-# Gera um tamanho aleatório para o vetor entre 1 e 20
+# Gera tamanho aleatório do vetor
 N=$((RANDOM % 20 + 1))
 
-# Cria o arquivo de entrada
-echo $N > input.txt
+# Arquivos específicos
+INPUT="vetor_input.txt"
+OUTPUT="vetor_output.txt"
 
-# Gera N números aleatórios entre -100 e 100
+# Cria entrada
+echo "$N" > "$INPUT"
 for ((i = 0; i < N; i++)); do
-    echo $((RANDOM % 201 - 100)) >> input.txt
+    echo $((RANDOM % 201 - 100)) >> "$INPUT"
 done
 
-# Compila o programa vetor
+# Compila o programa
 make vetor > /dev/null
 
-# Executa o programa com input.txt e salva a saída
-./vetor < input.txt > output.txt
+# Executa e salva saída
+./vetor < "$INPUT" > "$OUTPUT"
 
-# Exibe a entrada e saída
-echo "=== Entrada de teste (input.txt) ==="
-cat input.txt
-
-echo -e "\n=== Saída produzida pelo programa (output.txt) ==="
-cat output.txt
+# Mostra entrada e saída
+echo "=== Entrada ($INPUT) ==="
+cat "$INPUT"
+echo -e "\n=== Saída ($OUTPUT) ==="
+cat "$OUTPUT"
