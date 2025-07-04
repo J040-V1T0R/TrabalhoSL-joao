f8da2c851dd7ab3d1a9be75da043eb62f5c92730
J040-V1T0R <Joaovitordagracaamerico@gmail.com>
2025-07-04 00:41:03 -0300

    arrumando os scripts da matriz novamente

--- generate-test-matriz.sh
+++ generate-test-matriz.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Gera tamanho aleatório da matriz
+# Gera tamanho aleatório da matriz entre 2 e 5
 N=$((RANDOM % 4 + 2))
 
 # Operações possíveis
@@ -11,29 +11,33 @@ OPERACAO=${OPERACOES[$RANDOM % 3]}
 INPUT="matriz_input.txt"
 OUTPUT="matriz_output.txt"
 
-# Cria entrada
+# Inicia o arquivo com o valor de N
 echo "$N" > "$INPUT"
 
-# Primeira matriz
-for ((i = 0; i < N * N; i++)); do
-    echo $((RANDOM % 21 - 10)) >> "$INPUT"
-done
+# Função para gerar uma matriz N x N
+gerar_matriz() {
+    for ((i = 0; i < N; i++)); do
+        for ((j = 0; j < N; j++)); do
+            printf "%d " $((RANDOM % 21 - 10)) >> "$INPUT"
+        done
+        echo "" >> "$INPUT"  # Nova linha para próxima linha da matriz
+    done
+}
 
-# Segunda matriz
-for ((i = 0; i < N * N; i++)); do
-    echo $((RANDOM % 21 - 10)) >> "$INPUT"
-done
+# Gera duas matrizes
+gerar_matriz  # primeira
+gerar_matriz  # segunda
 
-# Operação
+# Adiciona a operação no final
 echo "$OPERACAO" >> "$INPUT"
 
 # Compila o programa
 make matriz > /dev/null
 
-# Executa e salva saída
+# Executa o programa com a entrada e salva a saída
 ./matriz < "$INPUT" > "$OUTPUT"
 
-# Mostra entrada e saída
+# Exibe entrada e saída
 echo "=== Entrada ($INPUT) ==="
 cat "$INPUT"
 echo -e "\n=== Saída ($OUTPUT) ==="
